8c867d2c4f95235de7d7e1e2c05489803274eb2d
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-12 23:24:04 +0000

    Second attempt

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,66 +1,40 @@
 // TODO: Write your class in this file
 
-
-//SoftPlay should have 3 functions: Occupancy, Enter and Leave.
-// SoftPlay should have the total number of adults (this.adults) and total number of children (this.children)
-// You want to use the if statement and Boolean true or false for this exercise.
-
-
-// Constructor with adults and children
 class SoftPlay {
   constructor (adults, children) {
-    this.totalAdultsInPlayground = 0;
-    this.totalChildrenInPlayground = 0;
+    this.totalAdults = 0;
+    this.totalChildren = 0;
   }
-  // Returns an object telling me the current number of adults and children in my softplay object.
+
   occupancy() {
     return {
-      adults: this.adults,
-      children: this.children
+      adults: this.totalAdults,
+      children: this.totalChildren
     }
   }
   enter (numAdults, numChildren) {
-    if (numAdults > 0) {
-      return true
-    }
-    // numAdults must be greater than 0
-    // If my check fail I return false
-    // If my check passes then I increment adults and children by the new ones. 
+    if (numAdults < numChildren) return false
+
+    if (numAdults > numChildren) return true
   }
-}
-
-// SoftPlay occupancy
-// Declare numAdults, numChild
-const softPlay =  (numAdults, numChild);
 
 
-//SoftPlay enter
-// Every child entering the softplay is with one adult (true). If not (false)
-enter (1, 1) {
-if (numAdults >= numChild) {
-  return true;
+leave (numAdults, numChildren) {
+  const expectedAdultCount = this.adults - numAdults
+  const expectedChildCount = this.children - numChildren
+
+  if (numChildren > numAdults) return false
+
+  if (expectedChildCount > expectedAdultCount) return false
+
+  if (expectedAdultCount < 0 || expectedChildCount < 0) return false
 } else {
-  return false;
+  this.totalAdults -= numAdults;
+  this.totalChildren -= numChildren;
+  return true
 }
-
-// SoftPlay leave
-// A child can't leave without an adult. 
-// A child will leave with one adult (true)
-// The number of adults and children leaving the center is <= adults and children staying in the center.
-
-leave () {
-  if (numAdults === 0 && numChild === 1) {
-    return false
-  } else {
-    return true
-  }
-}
-
-const softplay = new SoftPlay (5, 5);
-
 
 // TODO: Change undefined to the name of your class
 module.exports = {
   SoftPlay: SoftPlay
-}
 }
